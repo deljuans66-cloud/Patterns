76494f4845c6fcd17c6bd2ea67656c911fc9bb54
DelJuan Smith <deljuans66@gmail.com>
2025-10-20 19:01:10 -0400

    Test functions completed

--- patterns/accumulator.js
+++ patterns/accumulator.js
@@ -23,6 +23,15 @@ export function sumToN(n) {
  */
 export function factorial(n) {
   // TODO
+  if (typeof n !== "number") return NaN;
+  if (n < 0) return undefined;
+  if (n === 0) return 1;
+
+  let product = 1;
+  for (let i = 1; i <= n; i++) {
+    product *= i;
+  }
+  return product;
 }
 
 /**
@@ -33,6 +42,14 @@ export function factorial(n) {
  */
 export function buildNArray(n) {
   // TODO
+  if (typeof n !== "number") return null;
+  if (n <= 0) return [];
+
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(i);
+  }
+  return result;
 }
 
 /**
@@ -41,6 +58,13 @@ export function buildNArray(n) {
  */
 export function getLongestString(strings) {
   // TODO
+  let longest = "";
+  for (const str of strings) {
+    if (str.length > longest.length) {
+      longest = str;
+    }
+  }
+  return longest;
 }
 
 /**
@@ -49,6 +73,13 @@ export function getLongestString(strings) {
  */
 export function countPresent(attendance) {
   // TODO
+  let count = 0;
+  for (const present of attendance) {
+    if (present) {
+      count++;
+    }
+  }
+  return count;
 }
 
 /**
@@ -63,4 +94,27 @@ export function countPresent(attendance) {
  */
 export function complementDNA(dna) {
   // TODO
+  if (typeof dna !== "string") return null;
+
+  let complement = "";
+  for (const base of dna) {
+    switch (base) {
+      case "A":
+        complement += "T";
+        break;
+      case "T":
+        complement += "A";
+        break;
+      case "C":
+        complement += "G";
+        break;
+      case "G":
+        complement += "C";
+        break;
+      default:
+        // If an invalid character is found, we can choose to ignore it or handle it as needed
+        break;
+    }
+  }
+  return complement;
 }
